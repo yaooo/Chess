987c7da6b5e4dd4af428a9416bba8d7ea6c978be
yao <yaoshi616@gmail.com>
2017-10-25 02:34:57 -0400

    Update king.java

--- src/setup/King.java
+++ src/setup/King.java
@@ -9,18 +9,23 @@ public class King extends Piece {
     @Override
     public boolean isValidMove(String start,String input, Board board) {
 
-        // TODO King's special move
-
-
-
         int start_file = start.charAt(0) - 'a';
-        int start_rank = start.charAt(1) - '1';
         int dest_file = input.charAt(0) - 'a';
-        int dest_rank = input.charAt(1) - '1';
+        int start_rank=getRank(Integer.parseInt(start.charAt(1)+""));
+        int dest_rank=getRank(Integer.parseInt(input.charAt(1)+""));
 
         Square[][] b = board.getBoard();    //board is 8x8 matrix
         int diff_rank = dest_rank - start_rank;
         int diff_file = dest_file - start_file;
+
+        //TODO: Check for king's special move, has moved piece cannot do it
+        if(!this.hasMoved() && Math.abs(diff_rank) == 2 && dest_file == 0){
+
+                //TODO: Four different cases
+
+        }
+
+
 
         // Check the input parameters
         if(start.equals(input))
@@ -53,31 +58,80 @@ public class King extends Piece {
     }
 
     @Override
-    public void move(String start, String end, Board board) {
-        board.getSquare(start).setPiece(null);
-        board.getSquare(end).setPiece(this);
+    public void move(String startKingPos, String endKingPos, Board board) {
+        int start_file = startKingPos.charAt(0) - 'a';
+        int dest_file = endKingPos.charAt(0) - 'a';
+        int start_rank=getRank(Integer.parseInt(startKingPos.charAt(1)+""));
+        int dest_rank=getRank(Integer.parseInt(endKingPos.charAt(1)+""));
+
+        if(isValidMove(startKingPos,endKingPos, board)){
+            if(Math.abs(start_rank - dest_rank) <= 1) {
+                board.getSquare(startKingPos).setPiece(null);
+                board.getSquare(endKingPos).setPiece(this);
+            }
+
+            //TODO: the case of castling
+            //TODO: call castling(String startKingPos, String endKingPos, Board board)
+        }
     }
 
+    /**
+     * Do castling for king and return the board
+     * @param startKingPos  where the king starts
+     * @param endKingPos    where the king ends
+     * @param board The chessboard
+     * @return The updated chessboard
+     */
     public Square[][] castling(String startKingPos, String endKingPos, Board board) {
         int start_file = startKingPos.charAt(0) - 'a';
-        int start_rank = startKingPos.charAt(1) - '1';
         int dest_file = endKingPos.charAt(0) - 'a';
-        int dest_rank = endKingPos.charAt(1) - '1';
+        int start_rank=getRank(Integer.parseInt(startKingPos.charAt(1)+""));
+        int dest_rank=getRank(Integer.parseInt(endKingPos.charAt(1)+""));
 
         Square[][] b = board.getBoard();
 
-        if(!this.hasMoved()){
-
-
-
+        switch(startKingPos){
+            case "e8":
+                    //TODO
+                break;
+            case "e1":
+                    //TODO
+                break;
 
         }
-
-
-        //TODO: has moved piece cannot do it
-        //TODO: Add this to the validation check
-
+        return b;
     }
 
-
+    private int getRank(int j) {
+        int q=99;
+        switch(j) {
+            case 8:
+                q=0;
+                break;
+            case 7:
+                q=1;
+                break;
+            case 6:
+                q=2;
+                break;
+            case 5:
+                q=3;
+                break;
+            case 4:
+                q=4;
+                break;
+            case 3:
+                q=5;
+                break;
+            case 2:
+                q=6;
+                break;
+            case 1:
+                q=7;
+                break;
+            default:
+                q=-1;
+        }
+        return q;
+    }
 }
